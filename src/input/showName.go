d359b691f7370c309463450cf77e7d3314952fb9
YoNi25 <yoan.haouzi@gmail.com>
2021-04-04 13:29:25 +0200

    feat: Add version and source on the ShowName structure

--- src/input/showName.go
+++ src/input/showName.go
@@ -10,12 +10,15 @@ type ShowName struct {
 	TvShow   string
 	Season   string
 	Episode  string
+	Version  string
+	Source   string
 	Fullname string
 }
 
+const showNamePattern = "(?P<tvShow>.*)\\.(?P<season>S\\d+)(?P<episode>E\\d+).*-(?P<version>[^\\[]*)(\\[(?P<source>.*)\\])?"
 
 func buildShowName(showNameStr string) ShowName {
-	showNamePattern := regexp.MustCompile(`(?i)(?P<tvShow>.*)\.(?P<season>S\d+)(?P<episode>E\d+)`)
+	showNamePattern := regexp.MustCompile(`(?i)` + showNamePattern)
 
 	match := showNamePattern.FindStringSubmatch(showNameStr)
 	if len(match) == 0 {
@@ -31,9 +34,11 @@ func buildShowName(showNameStr string) ShowName {
 	}
 
 	return ShowName{
-		TvShow:   strings.Title(strings.ReplaceAll(result["tvShow"], ".", " ")),
-		Season:   strings.Title(result["season"]),
-		Episode:  strings.Title(result["episode"]),
-		Fullname: showNameStr,
+		strings.Title(strings.ReplaceAll(result["tvShow"], ".", " ")),
+		strings.Title(result["season"]),
+		strings.Title(result["episode"]),
+		strings.Title(result["version"]),
+		result["source"],
+		showNameStr,
 	}
 }
