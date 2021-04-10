4a8a7dae66cc010e619fca7b9122771b8b3d24ca
YoNi25 <yoan.haouzi@gmail.com>
2021-04-10 21:12:06 +0200

    refactor: Improve search implementation

--- src/downloader/search.go
+++ src/downloader/search.go
@@ -13,9 +13,21 @@ type searchSubtitle struct {
 	version  string
 }
 
-func retrieveShow(searchSubtitle searchSubtitle) (addic7ed.Subtitle, error) {
-	utils.Colors.Blue.Printf("\nLooking For subtitles for '%s'\n", searchSubtitle.name)
-	utils.Colors.Blue.Printf("The process could take some time\n\n")
+type subtitleSearcher struct {
+	colors   utils.ColorsStruct
+}
+
+// NewSubtitleSearcher return a new subtitleSearcher struct
+func NewSubtitleSearcher(colors utils.ColorsStruct) *subtitleSearcher {
+	construct := new(subtitleSearcher)
+	construct.colors = colors
+
+	return construct
+}
+
+func (subtitleSearch *subtitleSearcher) retrieveShow(searchSubtitle searchSubtitle) (addic7ed.Subtitle, error) {
+	subtitleSearch.colors.Blue.Printf("\nLooking For subtitles for '%s'\n", searchSubtitle.name)
+	subtitleSearch.colors.Blue.Printf("The process could take some time\n\n")
 
 	var warnings utils.Warnings
 
@@ -25,13 +37,13 @@ func retrieveShow(searchSubtitle searchSubtitle) (addic7ed.Subtitle, error) {
 		return addic7ed.Subtitle{}, &utils.Error{fmt.Sprintf("Fail to retrieve subtitles - %s", err)}
 	}
 
-	utils.Colors.Blue.Printf("ℹ️  TV Show %s found !\n", searchSubtitle.name)
-	utils.Colors.Blue.Println("------------------")
-	utils.Colors.Blue.Println(showName)          // Output: Shameless (US) - 08x11 - A Gallagher Pedicure
-	utils.Colors.Blue.Println(subtitle)          // Output: the best suitable subtitle in English language
-	utils.Colors.Blue.Println(subtitle.Version)  // Output: BATV
-	utils.Colors.Blue.Println(subtitle.Language) // Output: English
-	utils.Colors.Blue.Println("------------------")
+	subtitleSearch.colors.Blue.Printf("ℹ️  TV Show %s found !\n", searchSubtitle.name)
+	subtitleSearch.colors.Blue.Println("------------------")
+	subtitleSearch.colors.Blue.Println(showName)          // Output: Shameless (US) - 08x11 - A Gallagher Pedicure
+	subtitleSearch.colors.Blue.Println(subtitle)          // Output: the best suitable subtitle in English language
+	subtitleSearch.colors.Blue.Println(subtitle.Version)  // Output: BATV
+	subtitleSearch.colors.Blue.Println(subtitle.Language) // Output: English
+	subtitleSearch.colors.Blue.Println("------------------")
 
 	if !strings.Contains(strings.ToUpper(subtitle.Version), strings.ToUpper(searchSubtitle.version)) {
 		warnings = append(warnings, utils.Warning{fmt.Sprintf("The subtitle versions seem to be different. Check compatibility : %s", subtitle.Link)})
