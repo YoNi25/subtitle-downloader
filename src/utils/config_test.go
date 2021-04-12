fc8ddcedc89fa117167115cfae506d670dec971f
YoNi25 <yoan.haouzi@gmail.com>
2021-04-12 21:32:22 +0200

    refactor: Change config structure
    Rewrite the configuration structure by grouping elements :
    we now have a DirPaths and a Languages structure that contains availables
    dirPaths and Languages

---
+++ src/utils/config_test.go
@@ -0,0 +1,42 @@
+package utils
+
+import (
+	"github.com/stretchr/testify/assert"
+	"os"
+	"testing"
+)
+
+func Test_initializeConfig(t *testing.T) {
+	expectedConfig := Configuration{
+		DirPathsConfig: DirPathsConfig{
+			Available: map[string]string{
+				"Server":  "/a/server/dir/path",
+				"Desktop": "/a/desktop/dir/path",
+				"Current": ".",
+			},
+			Default: "/a/server/dir/path",
+		},
+		LanguagesConfig: LanguagesConfig{
+			Available: map[string]string{
+				"French":  "French",
+				"English": "English",
+			},
+			Default: "French",
+		},
+		SubtitleExtension: "srt",
+	}
+
+	config := initializeConfig()
+
+	assert.Equal(t, expectedConfig, config)
+}
+
+func Test_failWhenConfigFileDoesntExist(t *testing.T) {
+	os.Setenv("ENV", "env")
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("The code did not panic")
+		}
+	}()
+	_ = initializeConfig()
+}
